448efc042b8926df491b45dfbb5bb91d69725dfd
tar1971 <96209005+tar1971@users.noreply.github.com>
2022-05-13 05:43:17 +0200

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -6,7 +6,8 @@
 ###########################################
 #!/bin/sh
 echo
-
+rm -f /usr/lib/enigma2/Plugins/Extensions/xtraEvent
+wait
 opkg install --force-overwrite https://github.com/tar1971/xtraevent/blob/main/enigma2-plugin-extensions-xtraevent_v4.9_all.ipk?raw=true
 wait
 sleep 2;
